e84757d7c1176809b245cebea51c3c9e77453656
doro-git <sunhorse888@gmail.com>
2021-01-31 01:36:32 +0900

    index page changed

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Next.jsの練習サイトです。</p>
+        <p>Next.jsの練習サイトです(更新しました)。</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
